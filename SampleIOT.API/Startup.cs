fc373d5a79661456714439f1642db08b69d82c39
Zack <zkkzkk32312@msn.com>
2024-05-28 23:40:50 -0600

    add environment variables and corresponding cors settings

--- SampleIOT.API/Startup.cs
+++ SampleIOT.API/Startup.cs
@@ -5,14 +5,18 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SampleIOT.API.Services;
 using SampleIOT.API.Services.Interface;
+using System;
 
 namespace SampleIOT.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
@@ -23,15 +27,38 @@ namespace SampleIOT.API
             //services.AddCors();
             services.AddCors(options =>
             {
-                options.AddPolicy("AllowOrigin",
-                    builder =>
-                    {
-                        builder.WithOrigins("http://127.0.0.1:5500") // Adjust origin to match your frontend application
-                               .AllowCredentials()
-                               .AllowAnyMethod()
-                               .AllowAnyHeader();
-                    });
+                if (_env.IsDevelopment())
+                {
+                    options.AddPolicy("AllowLocalhost",
+                        builder =>
+                        {
+                            builder.SetIsOriginAllowed(origin =>
+                            {
+                                // If origin is null or empty, treat it as a local request
+                                if (string.IsNullOrEmpty(origin) || origin.ToLower() == "null")
+                                    return true;
+
+                                var uri = new Uri(origin);
+                                return uri.Host == "localhost" || uri.Host == "127.0.0.1";
+                            })
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowCredentials();
+                        });
+                }
+                else
+                {
+                    options.AddPolicy("AllowAnyOrigin",
+                        builder =>
+                        {
+                            builder
+                                .AllowAnyOrigin()
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                        });
+                }
             });
+
             services.AddControllers();
             services.AddSingleton<IDeviceService, DeviceService>();
             services.AddSingleton<ITelemetryService, TelemetryService>();
@@ -41,16 +68,14 @@ namespace SampleIOT.API
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, 
             IDeviceService deviceService, ITelemetryService telemetryService)
         {
-            app.UseCors("AllowOrigin");
-            //app.UseCors(builder =>
-            //    builder.AllowAnyOrigin()
-            //    .AllowAnyMethod()
-            //    .AllowAnyHeader()
-            //);
-
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseCors("AllowLocalhost");
+                //app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseCors("AllowAnyOrigin");
             }
 
             app.UseHttpsRedirection();
