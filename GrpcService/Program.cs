346141f7c34971f001b7731db0496b04a5bed800
Zack <zkkzkk32312@msn.com>
2024-08-02 17:51:04 -0600

    new Grpc project added

---
+++ GrpcService/Program.cs
@@ -0,0 +1,18 @@
+using GrpcService.Services;
+using SampleIOT.API.Services;
+using SampleIOT.API.Services.Interface;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddSingleton<IDeviceService, DeviceService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DeviceGrpcService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ GrpcService/Services/DeviceGrpcService .cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using GrpcService; // Namespace from `option csharp_namespace` in the .proto file
+using SampleIOT.API.Services.Interface;
+using System.Threading.Tasks;
+
+public class DeviceGrpcService : DeviceGrpc.DeviceGrpcBase
+{
+    private readonly IDeviceService _deviceService;
+
+    // Inject DeviceService via constructor
+    public DeviceGrpcService(IDeviceService deviceService)
+    {
+        _deviceService = deviceService;
+    }
+
+    // Implement the RPC method
+    public override async Task<DeviceReply> GetDeviceData(DeviceRequest request, ServerCallContext context)
+    {
+        // Use the injected DeviceService to fetch real data
+        var device = _deviceService.GetDevice(request.DeviceId);
+
+        // Map the device data to the response format
+        var response = new DeviceReply
+        {
+            DeviceId = device.Id,
+            DeviceType = device.Type,
+            TelemetryNames = { device.TelemetryNames }
+        };
+
+        return response;
+    }
+}
